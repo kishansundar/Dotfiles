a37bff3ca333cad007960b4bec3c404dc71f3581
Kishan <kishan@kishan.sh>
2020-03-15 20:18:18 +0530

    updating dotfiles

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git pull && git submodule update --init --recursive
 
 ln -sfn ~/.dotfiles/zshrc ~/.zshrc
-ln -sfn ~/.dotfiles/vimrc ~/.vimrc
+#ln -sfn ~/.dotfiles/vimrc ~/.vimrc
 ln -sfn ~/.dotfiles/hushlogin ~/.hushlogin
 ln -sfn ~/.dotfiles/gitconfig ~/.gitconfig
